de4531a0d8d2b2b2cbf164ef5a20977e6fc7e3fb
Atul Tiwari <tiwariatul9526@gmail.com>
2024-10-20 15:19:57 +0530

    upload files to cloudinary

--- elib-apis/src/book/bookRouter.ts
+++ elib-apis/src/book/bookRouter.ts
@@ -8,7 +8,8 @@ const bookRouter = express.Router();
 //file store local ->
 const upload = multer({
    dest: path.resolve(__dirname, "../../public/data/uploads"),
-   limits: { fileSize: 3e7 }, //30mb
+   //put limit 10 mb
+   limits: { fileSize: 1e7 }, //30mb
 });
 
 //routes
--- elib-apis/src/config/config.ts
+++ elib-apis/src/config/config.ts
@@ -1,11 +1,14 @@
-import { config as conf } from 'dotenv'
-conf()
+import { config as conf } from "dotenv";
+conf();
 
 const _config = {
-  port: process.env.PORT,
-  databaseURL: process.env.MONGO_CONNECTION_URL,
-  env: process.env.NODE_ENV,
-  jwtSecret: process.env.JWT_SECRET,
-}
+   port: process.env.PORT,
+   databaseURL: process.env.MONGO_CONNECTION_URL,
+   env: process.env.NODE_ENV,
+   jwtSecret: process.env.JWT_SECRET,
+   cloudinaryCloud: process.env.CLOUDINARY_CLOUD,
+   cloudinaryApiKey: process.env.CLOUDINARY_API_KEY,
+   cloudinaryApiSecret: process.env.CLOUDINARY_API_SECRET,
+};
 
-export const config = Object.freeze(_config)
+export const config = Object.freeze(_config);
