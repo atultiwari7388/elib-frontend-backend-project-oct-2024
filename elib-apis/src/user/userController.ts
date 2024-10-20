c867d79eae2f4960fd004cfe4ef94c13b3a82e88
Atul Tiwari <tiwariatul9526@gmail.com>
2024-10-20 13:29:12 +0530

    login endpoint done

--- elib-apis/src/user/userController.ts
+++ elib-apis/src/user/userController.ts
@@ -51,11 +51,78 @@ const createUser = async (req: Request, res: Response, next: NextFunction) => {
          algorithm: "HS256",
       });
       //response
-      res.json({ accessToken: token });
+      res.status(201).json({ accessToken: token });
    } catch (error) {
       console.error("Error in signin the jwt token:", error);
       return next(createHttpError(500, "Error while signing the jwt token"));
    }
 };
 
-export { createUser };
+// const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+//    const { email, password } = req.body;
+
+//    //validation
+//    if (!email || !password) {
+//       const err = createHttpError(400, "All Fields are required");
+//       return next(err);
+//    }
+
+//    //check users exists i db or not
+
+//    const user = await userModel.findOne({ email });
+
+//    if (!user) {
+//       return next(createHttpError(404, "User not found!"));
+//    }
+
+//    const isMatch = await bcrypt.compare(password, user.password);
+//    if (!isMatch) {
+//       return next(createHttpError(400, "Username or password incorrect"));
+//    }
+
+//    //create accesstoekn
+//    const token = sign({ sub: user._id }, config.jwtSecret as string, {
+//       expiresIn: "7d",
+//       algorithm: "HS256",
+//    });
+//    //response
+//    res.json({ accessToken: token });
+// };
+
+const loginUser = async (req: Request, res: Response, next: NextFunction) => {
+   const { email, password } = req.body;
+
+   // Validation
+   if (!email || !password) {
+      return next(createHttpError(400, "All Fields are required"));
+   }
+
+   try {
+      // Check if user exists in the database
+      const userExists = await userModel.findOne({ email });
+
+      if (!userExists) {
+         return next(createHttpError(404, "User not found!"));
+      }
+
+      // Compare passwords
+      const isMatch = await bcrypt.compare(password, userExists.password);
+      if (!isMatch) {
+         return next(createHttpError(400, "Username or password incorrect"));
+      }
+
+      // Create access token
+      const token = sign({ sub: userExists._id }, config.jwtSecret as string, {
+         expiresIn: "7d",
+         algorithm: "HS256",
+      });
+
+      // Successful response
+      res.status(200).json({ accessToken: token });
+   } catch (error) {
+      console.error("Error during login process:", error);
+      return next(createHttpError(500, "Internal Server Error"));
+   }
+};
+
+export { createUser, loginUser };
